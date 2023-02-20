963857bb10f572827e650347bc627973f20424f5
YI <sming0305@gmail.com>
2023-02-20 18:22:33 +0800

    W6-OK

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Vue-W6/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
